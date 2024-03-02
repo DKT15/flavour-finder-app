93758d0643c7af5bbbc70abb55479c6d6f3077d7
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-03-02 18:59:00 +0000

    changed around the formatting. Still working on the filter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import "./App.css";
 import Filter from "./components/Filter";
-import React, { useState, useEffect } from "react";
-import Card from "./Card";
 import "bootstrap/dist/css/bootstrap.min.css";
-import data from "./data.json";
-import "./App.css";
+import Restaurants from "./pages/restaurants";
 
 function App() {
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    setRestaurants(data.restaurants);
-  }, []);
-
   return (
     <>
       <Filter />
-      <div className="app">
-        {restaurants.map((restaurant) => (
-          <Card key={restaurant.restaurant_id} item={restaurant} />
-        ))}
-      </div>
-      <div className="card"></div>
+      <Restaurants />
     </>
   );
 }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,12 +3,26 @@ import "../App.css";
 import "../filter";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { event } from "jquery";
+import { data } from "../data";
+
+console.log(data);
 
 const Autocomplete = () => {
   const [value, setValue] = useState(null);
 
+  // {data
+  //   .filter((res) => {
+  //     return value.toLowerCase() === ""
+  //       ? res
+  //       : res.city.toLowerCase().includes(value);
+  //   })
+  //   .map((res) => ())}
+
   function handleSubmit(event) {
-    console.log(event);
+    //If the value is equal to the cards city, only show that elements/cards and hide the others.
+    // If(value === "London") {
+    //   console.log(event)
+    // }
   }
 
   return (
